310508f429ee38733526b2439465e4af5fad82a1
João Vitor Soares <jvfs12th@gmail.com>
2024-01-17 23:25:50 -0300

    :rocket: feat: Adiciona Interface e Repositorio de Usuarios

--- SistemaDeTarefas/Data/SistemaTarefasDBContext.cs
+++ SistemaDeTarefas/Data/SistemaTarefasDBContext.cs
@@ -12,5 +12,11 @@ namespace SistemaDeTarefas.Data
 
         public DbSet<UsuarioModel> Usuarios { get; set; }
         public DbSet<TarefaModel>  Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
