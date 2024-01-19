ea2d083d7977f5200e5e9bf848044c197a718e94
João Vitor Soares <jvfs12th@gmail.com>
2024-01-19 12:11:07 -0300

    :rocket: feat: Adiciona método de Listar tarefas

---
+++ SistemaDeTarefas/Controllers/TarefaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+        private readonly ITarefaRepositorio _tarefaRepositorio;
+        public TarefaController(ITarefaRepositorio tarefaRepositorio)
+        {
+            _tarefaRepositorio = tarefaRepositorio;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TarefaModel>>> ListarTodasTarefas()
+        {
+            List<TarefaModel> tarefas = await _tarefaRepositorio.BuscarTodasTarefas();
+            return Ok(tarefas);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioModel>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.BuscarPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
+            return Ok(usuario);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Editar([FromBody] UsuarioModel usuarioModel, int id)
+        {
+            usuarioModel.Id = id;
+            UsuarioModel usuario = await _usuarioRepositorio.Editar(usuarioModel, id);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+        {
+            bool apagado = await _usuarioRepositorio.Apagar(id);
+            return Ok(apagado);
+        }
+
+    }
+}
