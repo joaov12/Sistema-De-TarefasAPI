63b9d97950958d28be4c16a0f79ad956a0379883
João Vitor Soares <jvfs12th@gmail.com>
2024-01-19 11:48:10 -0300

    :construction_worker: build: Cria a interface ITarefaRepositório

---
+++ SistemaDeTarefas/Repositorios/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BuscarPorId(int id);
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> Editar(UsuarioModel usuario, int id);
+        Task<bool> Apagar(int id);
+    }
+}
